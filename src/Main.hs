344ac584f77863139d5e9cb0a157bb90f0e53da0
zimooo2 <zimooo2@gmail.com>
2020-02-11 22:33:52 -0800

    include prelude from source dir

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ import Parser
 import Lexer
 import Compiler
 import Ast
+import Control.Exception
 -- import TypeChecker
 
 type Env = String -> [Stmt]
@@ -10,13 +11,18 @@ emptyEnv = error "Not found"
 envLookup s env = env s
 envBind s v env = (\s' -> if s == s' then v else env s)
 
+getFileContent :: String -> FilePath -> IO String
+getFileContent def filePath = readFile filePath `catch`
+    \e -> const (return def) (e :: IOException)
+
 main :: IO ()
 main = do
-    s <- getContents
-    let parsedEither = parse s
+    file <- getContents
+    prelude <- getFileContent "" "prelude.bgl"
+    let parsedEither = parse $ file ++ "\n\n\n-- PRELUDE\n" ++ prelude
 
     case parsedEither of
-        Left s -> putStrLn s
+        Left err -> putStrLn err
         Right ast -> do
             print ast
             writeFile "output.hs" (compile ast)

cc996e9e2b572da54702bc5ecbd8b5442e196580
zimooo2 <zimooo2@gmail.com>
2020-03-09 16:11:41 -0700

    made it so you don't need to pipe

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import Compiler
 import Ast
 --import TypeChecker
 import Control.Exception
+import System.Environment   
 
 type Env = String -> [Stmt]
 emptyEnv = error "Not found"
@@ -16,35 +17,41 @@ getFileContent def filePath = readFile filePath `catch`
     \e -> const (return def) (e :: IOException)
 
 main :: IO ()
-main = do
-    file <- getContents
-    prelude <- getFileContent "" "prelude.bgl"
-    let parsedEitherFile = parse $ file
-    let parsedEitherPrelude = parse $ prelude
-    print parsedEitherFile
-    
-    case parsedEitherFile of
-        Left err -> putStrLn "Error in boardgame code" >> putStrLn err
-        Right ast -> do
-            print ast
-            --let (valid, error) = check_start ast
-                --in if(valid) 
-                        --then   
-            writeFile "OutputCode.hs" (compile ast) >> writeFile "OutputBuiltins.hs" (compile_builtin ast)
-                        --else   putStrLn error   
-    case parsedEitherPrelude of
-        Left err -> putStrLn "Error in prelude" >> putStrLn err
-        Right ast -> do
-            print ast
-            --let (valid, error) = check_start ast
-                --in if(valid) 
-                        --then   
-            appendFile "OutputBuiltins.hs" ("\n\n -- Prelude \n" ++ 
-                    compile_prelude ast)
-                        --else   putStrLn error
+main =
+    -- read commmand line arguments
+    getArgs >>= \args ->
+    if length args /= 1 
+    then
+        putStrLn "Error: Please provide exactly one file to compile"
+    else do 
+        file <- getFileContent "" $ args !! 0
+        prelude <- getFileContent "" "prelude.bgl"
+        let parsedEitherFile = parse $ file
+        let parsedEitherPrelude = parse $ prelude
+        print parsedEitherFile
+        
+        case parsedEitherFile of
+            Left err -> putStrLn "Error in boardgame code" >> putStrLn err
+            Right ast -> do
+                print ast
+                --let (valid, error) = check_start ast
+                    --in if(valid) 
+                            --then   
+                writeFile "OutputCode.hs" (compile ast) >> writeFile "OutputBuiltins.hs" (compile_builtin ast)
+                            --else   putStrLn error   
+        case parsedEitherPrelude of
+            Left err -> putStrLn "Error in prelude" >> putStrLn err
+            Right ast -> do
+                print ast
+                --let (valid, error) = check_start ast
+                    --in if(valid) 
+                            --then   
+                appendFile "OutputBuiltins.hs" ("\n\n -- Prelude \n" ++ 
+                        compile_prelude ast)
+                            --else   putStrLn error
 
-    -- let (valid, error) = check_start(ast)
-    -- putStrLn error
-   
-    --print (run ast)
+        -- let (valid, error) = check_start(ast)
+        -- putStrLn error
+    
+        --print (run ast)
     
